00726e44b5df36fbd731dc1259884d6e2e098b94
liuyang19900520 <liuyang19900520@hotmail.com>
2018-05-21 19:12:09 +0900

    朴素贝叶斯- 进行文本分类

--- ML4/ML4__init__.py
+++ ML4/ML4__init__.py
@@ -1,9 +1,27 @@
 import ML4.bayes as bayes
 
-listOPosts, listClasses = bayes.loadDataSet()
-myVocabList=bayes.createVocabList(listOPosts)
-print(myVocabList)
-r1=bayes.setOfWords2Vec(myVocabList,listOPosts[0])
+# # 切割后的文本与标签
+# listOPosts, listClasses = bayes.loadDataSet()
+# #创建不重复词表
+# myVocabList=bayes.createVocabList(listOPosts)
+# print(myVocabList)
+#
+# trainMat = []
+# # postinDoc是每一个词汇的数组
+# for postinDoc in listOPosts:
+#     #append 中的内容是：输入的文档在词汇表中是否在文档中出现
+#     #同时将向量添加到trainMat中，trainMat的数组中包含了
+#     trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
+#
+# print(trainMat)
+#
+# p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
+#
+# print("p0V代表了这个词汇出现在正常语句中的频率")
+# print(p0V)
+# print("p1V代表了这个词汇出现在侮辱性语句中的频率")
+# print(p1V)
+# print(pAb)
 
-print(r1)
 
+bayes.testingNB();
--- ML4/bayes.py
+++ ML4/bayes.py
@@ -45,3 +45,89 @@ def setOfWords2Vec(vocabList, inputSet):
         else:
             print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
+
+
+#
+# 词袋模型
+# 和原有的模型的差别在于+=1 这一块
+# 如果输入的文档中的词汇在vocablist中存在就设置为1，现在是出现就累加，
+# 在词集中，每个单词只能出现一次，而在词袋中，可以出现多次
+#
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
+
+#
+# trainMatrix：文档矩阵（postingList中的每个元素在my Vocabulary中是否出现的情况）
+# trainCategory：每篇文章类别标签构成的向量
+#
+#
+def trainNB0(trainMatrix, trainCategory):
+    # 计算每个类别中的文档个数
+    numTrainDocs = len(trainMatrix)
+    # 计算my Vocabulary的个数
+    numWords = len(trainMatrix[0])
+    # sum(测试分类的和)/测试分类的数量
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # 分母变量是一个元素个数等于词汇表大小的NumPy数组。
+    # sum(trainCategory)是向量为1的与文档个数总作比较得出pbAusive
+    # 初始化概率(p0 代表0（正常）词条，p1代表1（侮辱）词条，出现文档中的词条# )
+    p0Num = ones(numWords);
+    p1Num = ones(numWords)  # change to ones()
+    p0Denom = 2.0;
+    p1Denom = 2.0  # change to 2.0
+
+    for i in range(numTrainDocs):
+        # 如果词条出现在文档中, 增加该词条的计数值
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]  # 将传递的参数（文档矩阵）自加，得到在总词条中出现的个数的矩阵
+            p1Denom += sum(trainMatrix[i])  # 这里是矩阵值相加，最后得出矩阵中元素出现的次数和
+        # 增加所有词条的计数值
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 对于每个元素/该类别的总元素（矩阵/向量，这个词出现在侮辱性语句中的频率）
+    p1Vect = log(p1Num / p1Denom)  # change to log()
+    p0Vect = log(p0Num / p0Denom)  # change to log()
+    return p0Vect, p1Vect, pAbusive
+
+
+#
+# 朴素贝叶斯函数
+# vec2Classify:分类向量
+# p0Vec:0的概率
+# p1Vec 1的概率
+# pClass1: 在分类中1的概率
+#
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # element-wise mult
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    # 切割后的文本与标签
+    listOPosts, listClasses = loadDataSet()
+    # 创建不重复词表
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    # postinDoc是每一个词汇的数组
+    for postinDoc in listOPosts:
+        # append 中的内容是：输入的文档在词汇表中是否在文档中出现
+        # 同时将向量添加到trainMat中，trainMat的数组中包含了
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    # 分别得到0的概率 1的概率 分类中出现1的概率
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
